a339707003841d3e662b8aa908321173fedef41a
rohit dharmik <rohit1234dharmik@gmail.com>
2025-12-11 13:38:34 +0530

    deploy update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
- 
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: "0.0.0.0", // Allow access from external devices during local development
+    port: 8080, // Port for local development, Netlify and Vercel handle this for production
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // This should be 'dist' as this is the default output for Vite
+    // Optionally, set target or other build options if needed
+    target: "esnext", // Adjust target for modern browsers
   },
 }));
